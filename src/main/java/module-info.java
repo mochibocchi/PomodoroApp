f6f43775268518af78f554373ebab6347894dfc4
Morningstar5426 <walwick10@gmail.com>
2024-04-29 16:08:58 +1000

    Merge branch 'refs/heads/master' into master_test
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/workspace.xml
    #	src/main/java/com/example/pomodoroapp/HelloApplication.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.pomodoroapp {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.pomodoroapp to javafx.fxml;
     exports com.example.pomodoroapp;
