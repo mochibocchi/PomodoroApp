2d1a37c4013a32c804723e7c9e28b7cd6b44d870
Timmy <116723040+mochibocchi@users.noreply.github.com>
2024-05-14 14:52:44 +1000

    add helloController back, also force install sqlite jdbc

---
+++ src/main/java/com/example/pomodoroapp/Controller/HelloController.java
@@ -0,0 +1,10 @@
+    package com.example.pomodoroapp.Controller;
+
+    import javafx.fxml.FXML;
+    import javafx.scene.control.Label;
+
+    public class HelloController {
+        @FXML
+        private Label welcomeText;
+
+    }
